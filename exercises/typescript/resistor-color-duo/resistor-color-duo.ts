b8adfb402b573e173ce06f1ba895155350cbb72f
GoFightNguyen <jsn.dev@outlook.com>
2023-01-15 13:57:39 -0700

    exercise(resistor-color-duo): switch from Record to enum to constrain inputs

--- exercises/typescript/resistor-color-duo/resistor-color-duo.ts
+++ exercises/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,17 +1,20 @@
-export function decodedValue([color1, color2]: string[]): number {
-  const resistorBandEncodings: Record<string, string> = {
-    black: '0',
-    brown: '1',
-    red: '2',
-    orange: '3',
-    yellow: '4',
-    green: '5',
-    blue: '6',
-    violet: '7',
-    grey: '8',
-    white: '9',
-  };
-  const resistance1 = resistorBandEncodings[color1];
-  const resistance2 = resistorBandEncodings[color2];
+enum ResistorBandEncodings {
+  black = '0',
+  brown = '1',
+  red = '2',
+  orange = '3',
+  yellow = '4',
+  green = '5',
+  blue = '6',
+  violet = '7',
+  grey = '8',
+  white = '9',
+}
+
+type Color = keyof typeof ResistorBandEncodings;
+
+export function decodedValue([color1, color2]: Color[]): number {
+  const resistance1 = ResistorBandEncodings[color1];
+  const resistance2 = ResistorBandEncodings[color2];
   return parseInt(resistance1 + resistance2);
 }
