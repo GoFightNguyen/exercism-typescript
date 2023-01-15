0aed0464b734708b791b4cd006d6fcb9681a653b
GoFightNguyen <jsn.dev@outlook.com>
2023-01-15 12:16:34 -0700

    exercise(resistor-color-duo): complete

---
+++ exercises/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,17 @@
+export function decodedValue([color1, color2]: string[]): number {
+  const resistorBandEncodings: Record<string, string> = {
+    black: '0',
+    brown: '1',
+    red: '2',
+    orange: '3',
+    yellow: '4',
+    green: '5',
+    blue: '6',
+    violet: '7',
+    grey: '8',
+    white: '9',
+  };
+  const resistance1 = resistorBandEncodings[color1];
+  const resistance2 = resistorBandEncodings[color2];
+  return Number.parseInt(resistance1 + resistance2);
+}
