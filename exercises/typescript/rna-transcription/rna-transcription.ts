9f06d010fffac00441a57ec6d8e57dc54eb05ef2
GoFightNguyen <jsn.dev@outlook.com>
2023-01-22 12:19:59 -0700

    exercise(rna-transcription): narrow input to an array of nucleotides asap

--- exercises/typescript/rna-transcription/rna-transcription.ts
+++ exercises/typescript/rna-transcription/rna-transcription.ts
@@ -1,6 +1,9 @@
 type DNA_Nucleotides = 'G' | 'C' | 'T' | 'A';
 type RNA_Nucleotides = 'C' | 'G' | 'A' | 'U';
+type DnaStrand = DNA_Nucleotides[];
 
+// using a record at least ensures that all DNA_Nucleotides are used.
+// Could have been a map, but the error when missing a DNA_Nucleotides was harder to read
 const DNA_TO_RNA: Record<DNA_Nucleotides, RNA_Nucleotides> = {
   G: 'C',
   C: 'G',
@@ -8,13 +11,15 @@ const DNA_TO_RNA: Record<DNA_Nucleotides, RNA_Nucleotides> = {
   A: 'U',
 };
 
-export function toRna(dnaStrand: string): string {
+export function toRna(dnaInput: string): string {
   const regex = new RegExp('^[GCTA]+$');
-  if (!regex.test(dnaStrand)) throw new Error('Invalid input DNA.');
+  if (!regex.test(dnaInput)) throw new Error('Invalid input DNA.');
+  const dnaStrand = dnaInput as unknown as DnaStrand;
 
   let rnaStrand = '';
   for (const dnaNucleotide of dnaStrand) {
-    rnaStrand += DNA_TO_RNA[dnaNucleotide as DNA_Nucleotides];
+    rnaStrand += DNA_TO_RNA[dnaNucleotide];
   }
+
   return rnaStrand;
 }
