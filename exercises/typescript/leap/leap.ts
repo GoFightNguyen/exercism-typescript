c3a8d856dee5202f76a63196f2143000e8a80496
GoFightNguyen <jsn.dev@outlook.com>
2023-01-22 10:14:42 -0700

    exercise(leap): consolidate return statements

--- exercises/typescript/leap/leap.ts
+++ exercises/typescript/leap/leap.ts
@@ -1,6 +1,8 @@
 export function isLeap(year: number): boolean {
-  if (isEvenlyDivisable(year, 400)) return true;
-  return isEvenlyDivisable(year, 4) && !isEvenlyDivisable(year, 100);
+  return (
+    isEvenlyDivisable(year, 400) ||
+    (isEvenlyDivisable(year, 4) && !isEvenlyDivisable(year, 100))
+  );
 }
 
 function isEvenlyDivisable(dividend: number, divisor: number): boolean {
