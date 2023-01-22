a9708bf504c083af8183b87301cf55b02ffb9cf9
GoFightNguyen <jsn.dev@outlook.com>
2023-01-22 11:57:36 -0700

    exercise(rna-transcription): use a Record for the mapping

--- exercises/typescript/rna-transcription/rna-transcription.ts
+++ exercises/typescript/rna-transcription/rna-transcription.ts
@@ -1,22 +1,20 @@
+type DNA_Nucleotides = 'G' | 'C' | 'T' | 'A';
+type RNA_Nucleotides = 'C' | 'G' | 'A' | 'U';
+
+const DNA_TO_RNA: Record<DNA_Nucleotides, RNA_Nucleotides> = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
+};
+
 export function toRna(dnaStrand: string): string {
+  const regex = new RegExp('^[GCTA]+$');
+  if (!regex.test(dnaStrand)) throw new Error('Invalid input DNA.');
+
   let rnaStrand = '';
   for (const dnaNucleotide of dnaStrand) {
-    rnaStrand += toRnaNucleotide(dnaNucleotide);
+    rnaStrand += DNA_TO_RNA[dnaNucleotide as DNA_Nucleotides];
   }
   return rnaStrand;
-}
-
-function toRnaNucleotide(dnaNucleotide: string): string {
-  switch (dnaNucleotide) {
-    case 'C':
-      return 'G';
-    case 'G':
-      return 'C';
-    case 'A':
-      return 'U';
-    case 'T':
-      return 'A';
-    default:
-      throw new Error('Invalid input DNA.');
-  }
 }
