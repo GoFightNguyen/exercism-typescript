3b09d4edacd12a96de37acb1f2a480f0fc03b8cf
GoFightNguyen <jsn.dev@outlook.com>
2023-01-22 10:43:01 -0700

    exercise(resistor-color): constrain color input by using const assertions and index access types

--- exercises/typescript/resistor-color/resistor-color.ts
+++ exercises/typescript/resistor-color/resistor-color.ts
@@ -1,3 +1,7 @@
+/**
+ * `as const` (const assertion) changes the type from string[] to a literal type
+ * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
+ */
 export const COLORS = [
   'black',
   'brown',
@@ -9,8 +13,17 @@ export const COLORS = [
   'violet',
   'grey',
   'white',
-];
+] as const;
 
-export const colorCode = (color: string): number => {
+/**
+ * create a  type of the array COLORS accessed at any index (Index Access Types).
+ * since number can take any numeric value,
+ * the type created is a union type of all the possible accessed entries of the array.
+ *
+ * https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+ */
+type Color = typeof COLORS[number];
+
+export const colorCode = (color: Color): number => {
   return COLORS.indexOf(color);
 };
