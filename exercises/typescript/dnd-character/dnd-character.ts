7ee8331547686403a39aa28af01f9de371ad898a
GoFightNguyen <jsn.dev@outlook.com>
2023-01-24 17:23:00 -0700

    exercise(dnd-character): complete

---
+++ exercises/typescript/dnd-character/dnd-character.ts
@@ -0,0 +1,37 @@
+export class DnDCharacter {
+  public readonly constitution = 3;
+
+  public hitpoints: number;
+  public strength: number;
+  public dexterity: number;
+  public intelligence: number;
+  public wisdom: number;
+  public charisma: number;
+
+  constructor() {
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+  }
+
+  public static generateAbilityScore(): number {
+    const dieValues = [rollDice(), rollDice(), rollDice(), rollDice()]
+      .sort()
+      .slice(1);
+
+    return dieValues.reduce((sum, addend) => sum + addend, 0);
+  }
+
+  public static getModifierFor(abilityValue: number): number {
+    return roundDownToNearestInt((abilityValue - 10) / 2);
+  }
+}
+
+function rollDice(): number {
+  return roundDownToNearestInt(Math.random() * 7);
+}
+
+const roundDownToNearestInt = (value: number) => Math.floor(value);
