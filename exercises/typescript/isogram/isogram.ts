9b559d72fa4b1e3a2ae82ca4eddb6b86956b6fdb
GoFightNguyen <jsn.dev@outlook.com>
2023-01-29 16:48:04 -0700

    exercise(isogram): use size rather than reconstruct string

--- exercises/typescript/isogram/isogram.ts
+++ exercises/typescript/isogram/isogram.ts
@@ -1,7 +1,5 @@
 export function isIsogram(candidate: string): boolean {
   candidate = candidate.replace(/\s+|\-+/, '').toLocaleLowerCase();
   const uniqueCharacters = new Set([...candidate]);
-  let temp = '';
-  uniqueCharacters.forEach((c) => (temp += c));
-  return candidate === temp;
+  return uniqueCharacters.size === candidate.length;
 }
