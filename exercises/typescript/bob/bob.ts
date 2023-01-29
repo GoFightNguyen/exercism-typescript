4945c4377e3670f128a10da0ecd1217fbb2f4b7b
GoFightNguyen <jsn.dev@outlook.com>
2023-01-29 11:51:51 -0700

    exercise(bob): complete

---
+++ exercises/typescript/bob/bob.ts
@@ -0,0 +1,21 @@
+const isShouting = (message: string): boolean => {
+  const hasLetters = /[a-z]/i;
+  const hasAnyLowercaseLetters = /[a-z]/;
+  return hasLetters.test(message) && !hasAnyLowercaseLetters.test(message);
+};
+
+const isQuestion = (message: string): boolean => Boolean(message.match(/\?$/));
+const isSilent = (message: string): boolean => !message;
+
+export function hey(message: string): string {
+  message = message.trim();
+  if (isSilent(message)) return 'Fine. Be that way!';
+
+  if (isShouting(message)) {
+    return isQuestion(message)
+      ? "Calm down, I know what I'm doing!"
+      : 'Whoa, chill out!';
+  }
+
+  return isQuestion(message) ? 'Sure.' : 'Whatever.';
+}
