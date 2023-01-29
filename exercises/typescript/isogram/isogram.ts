37f4ccd8252ea5cf9b1c953286ae62e813f8e0fd
GoFightNguyen <jsn.dev@outlook.com>
2023-01-29 16:44:34 -0700

    exercise(isogram): use regex to replace spaces and hypens

--- exercises/typescript/isogram/isogram.ts
+++ exercises/typescript/isogram/isogram.ts
@@ -1,8 +1,5 @@
 export function isIsogram(candidate: string): boolean {
-  candidate = candidate
-    .replaceAll('-', '')
-    .replaceAll(' ', '')
-    .toLocaleLowerCase();
+  candidate = candidate.replace(/\s+|\-+/, '').toLocaleLowerCase();
   const uniqueCharacters = new Set([...candidate]);
   let temp = '';
   uniqueCharacters.forEach((c) => (temp += c));
