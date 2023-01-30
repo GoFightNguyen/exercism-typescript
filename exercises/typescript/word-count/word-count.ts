9593c54f478dfba09b621905c29c735b059f3b40
GoFightNguyen <jsn.dev@outlook.com>
2023-01-29 17:48:28 -0700

    exercise(word-count): use regex to match all not-space

--- exercises/typescript/word-count/word-count.ts
+++ exercises/typescript/word-count/word-count.ts
@@ -1,13 +1,12 @@
 export function count(phrase: string): Map<string, number> {
+  const counts = new Map<string, number>();
   const words = phrase
     .toLocaleLowerCase()
-    .trim()
-    .replace(/\s+/, ' ')
-    .split(' ');
-  const counts = new Map<string, number>();
-  words.forEach((w) => {
-    let count = counts.get(w) ?? 0;
-    counts.set(w, count + 1);
-  });
+    .match(/\S+/g)
+    ?.forEach((w) => {
+      let count = counts.get(w) ?? 0;
+      counts.set(w, count + 1);
+    });
+    
   return counts;
 }
