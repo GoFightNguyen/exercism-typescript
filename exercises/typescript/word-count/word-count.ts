c6bb66cabbbfc44868427a44dfdeecb9cdef6c4a
GoFightNguyen <jsn.dev@outlook.com>
2023-01-30 19:25:49 -0700

    exercise(word-count): cleanup

--- exercises/typescript/word-count/word-count.ts
+++ exercises/typescript/word-count/word-count.ts
@@ -1,11 +1,11 @@
 export function count(phrase: string): Map<string, number> {
   const counts = new Map<string, number>();
-  const words = phrase
+  phrase
     .toLocaleLowerCase()
     .match(/\S+/g)
     ?.forEach((w) => {
-      let count = counts.get(w) ?? 0;
-      counts.set(w, count + 1);
+      let c = counts.get(w) ?? 0;
+      counts.set(w, c + 1);
     });
     
   return counts;
