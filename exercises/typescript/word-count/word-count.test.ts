861f3ffae9866f528b40bd2d8dd88ebbfd3cdb5f
GoFightNguyen <jsn.dev@outlook.com>
2023-01-30 19:24:01 -0700

    exercise(word-count): remove trailing ; from tests

--- exercises/typescript/word-count/word-count.test.ts
+++ exercises/typescript/word-count/word-count.test.ts
@@ -1,24 +1,24 @@
-import { count } from './word-count';
+import { count } from './word-count'
 
 describe('words()', () => {
   it('counts one word', () => {
-    const expectedCounts = new Map(Object.entries({ word: 1 }));
-    expect(count('word')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ word: 1 }))
+    expect(count('word')).toEqual(expectedCounts)
+  })
 
   it('counts one of each', () => {
-    const expectedCounts = new Map(Object.entries({ one: 1, of: 1, each: 1 }));
-    expect(count('one of each')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ one: 1, of: 1, each: 1 }))
+    expect(count('one of each')).toEqual(expectedCounts)
+  })
 
   it('counts multiple occurrences', () => {
     const expectedCounts = new Map(
       Object.entries({ one: 1, fish: 4, two: 1, red: 1, blue: 1 })
-    );
+    )
     expect(count('one fish two fish red fish blue fish')).toEqual(
       expectedCounts
-    );
-  });
+    )
+  })
 
   it('includes punctuation', () => {
     const expectedCounts = new Map(
@@ -30,50 +30,50 @@ describe('words()', () => {
         java: 1,
         'javascript!!&@$%^&': 1,
       })
-    );
+    )
     expect(count('car : carpet as java : javascript!!&@$%^&')).toEqual(
       expectedCounts
-    );
-  });
+    )
+  })
 
   it('includes numbers', () => {
-    const expectedCounts = new Map(Object.entries({ testing: 2, 1: 1, 2: 1 }));
-    expect(count('1 2 testing testing')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ testing: 2, 1: 1, 2: 1 }))
+    expect(count('1 2 testing testing')).toEqual(expectedCounts)
+  })
 
   it('normalizes to lower case', () => {
-    const expectedCounts = new Map(Object.entries({ go: 3 }));
-    expect(count('go Go GO')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ go: 3 }))
+    expect(count('go Go GO')).toEqual(expectedCounts)
+  })
 
   it('counts properly international characters', () => {
     const expectedCounts = new Map(
       Object.entries({ '¡hola!': 1, '¿qué': 1, 'tal?': 1, 'привет!': 1 })
-    );
-    expect(count('¡Hola! ¿Qué tal? Привет!')).toEqual(expectedCounts);
-  });
+    )
+    expect(count('¡Hola! ¿Qué tal? Привет!')).toEqual(expectedCounts)
+  })
 
   it('counts multiline', () => {
-    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }));
-    expect(count('hello\nworld')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
+    expect(count('hello\nworld')).toEqual(expectedCounts)
+  })
 
   it('counts tabs', () => {
-    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }));
-    expect(count('hello\tworld')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
+    expect(count('hello\tworld')).toEqual(expectedCounts)
+  })
 
   it('counts multiple spaces as one', () => {
-    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }));
-    expect(count('hello  world')).toEqual(expectedCounts);
-  });
+    const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
+    expect(count('hello  world')).toEqual(expectedCounts)
+  })
 
   it('does not count leading or trailing whitespace', () => {
     const expectedCounts = new Map(
       Object.entries({ introductory: 1, course: 1 })
-    );
-    expect(count('\t\tIntroductory Course      ')).toEqual(expectedCounts);
-  });
+    )
+    expect(count('\t\tIntroductory Course      ')).toEqual(expectedCounts)
+  })
 
   it('handles properties that exist on Object’s prototype', () => {
     const expectedCounts = new Map(
@@ -86,9 +86,9 @@ describe('words()', () => {
         tostring: 1,
         'ok?': 1,
       })
-    );
+    )
     expect(count('reserved words like constructor and toString ok?')).toEqual(
       expectedCounts
-    );
-  });
-});
+    )
+  })
+})
