469aca83d1a6d18c426e1ca132c4fd7d08762c10
GoFightNguyen <jsn.dev@outlook.com>
2023-02-05 16:09:26 -0700

    exercise(raindrops): functional

--- exercises/typescript/raindrops/raindrops.ts
+++ exercises/typescript/raindrops/raindrops.ts
@@ -1,7 +1,11 @@
+const isAFactor = (num: number, sound: string) => (value: number) =>
+  value % num ? '' : sound;
+
+const is3AFactor = isAFactor(3, 'Pling');
+const is5AFactor = isAFactor(5, 'Plang');
+const is7AFactor = isAFactor(7, 'Plong');
+
 export function convert(value: number): string {
-  let sounds = '';
-  if (!(value % 3)) sounds += 'Pling';
-  if (!(value % 5)) sounds += 'Plang';
-  if (!(value % 7)) sounds += 'Plong';
+  let sounds = is3AFactor(value) + is5AFactor(value) + is7AFactor(value);
   return sounds === '' ? String(value) : sounds;
 }
