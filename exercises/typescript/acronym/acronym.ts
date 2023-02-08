4a20f7cdc780e535a1945012f7fe9304a516356b
GoFightNguyen <jsn.dev@outlook.com>
2023-02-08 08:30:28 -0700

    exercise(acronym): use match() to avoid map() and split()

--- exercises/typescript/acronym/acronym.ts
+++ exercises/typescript/acronym/acronym.ts
@@ -2,8 +2,6 @@ export function parse(phrase: string): string {
   const indexOfColon = phrase.indexOf(':');
   if (indexOfColon >= 0) return phrase.substring(0, indexOfColon);
 
-  return phrase
-    .split(/[\s-]|(?=[A-Z])/)
-    .map((w) => w[0].toUpperCase())
-    .join('');
+  const initials = phrase.match(/\b(\w)|[A-Z]/g) ?? [];
+  return initials.join('').toUpperCase();
 }
