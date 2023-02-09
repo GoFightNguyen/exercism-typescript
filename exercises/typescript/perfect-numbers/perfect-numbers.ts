2086ee2f650aa902a90f1e0705cf899ef0deeaf7
GoFightNguyen <jsn.dev@outlook.com>
2023-02-08 18:07:49 -0700

    exercise(perfect-numbers): rename getFactorsOf to findFactorsOf

--- exercises/typescript/perfect-numbers/perfect-numbers.ts
+++ exercises/typescript/perfect-numbers/perfect-numbers.ts
@@ -1,6 +1,6 @@
 type NICOMACHUS_CLASSIFICATION = 'perfect' | 'abundant' | 'deficient';
 
-function getFactorsOf(num: number): number[] {
+function findFactorsOf(num: number): number[] {
   let factors: number[] = [];
   for (let candidate = 1; candidate < num; candidate++) {
     if (num % candidate === 0) factors.push(candidate);
@@ -15,7 +15,7 @@ export function classify(num: number): NICOMACHUS_CLASSIFICATION {
   if (num < 1)
     throw new Error('Classification is only possible for natural numbers.');
 
-  let factors = getFactorsOf(num);
+  let factors = findFactorsOf(num);
   let aliquotSum = sum(factors);
   if (aliquotSum === num) return 'perfect';
   else if (aliquotSum > num) return 'abundant';
