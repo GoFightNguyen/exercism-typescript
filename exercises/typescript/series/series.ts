27fe3a6030f5ce0862b205321dc1eac8b84301ba
GoFightNguyen <jsn.dev@outlook.com>
2023-02-17 15:40:58 -0700

    exercise(series): extract validateSliceLenth

--- exercises/typescript/series/series.ts
+++ exercises/typescript/series/series.ts
@@ -1,15 +1,10 @@
 export class Series {
   constructor(private readonly series: string) {
-    if(!series) throw new Error('series cannot be empty')
+    if (!series) throw new Error('series cannot be empty');
   }
 
   slices(sliceLength: number): number[][] {
-    if (sliceLength > this.series.length)
-      throw new Error('slice length cannot be greater than series length');
-
-    if (sliceLength === 0) throw new Error('slice length cannot be zero');
-
-    if(sliceLength < 0) throw new Error('slice length cannot be negative');
+    this.validateSliceLength(sliceLength);
 
     const allSeries: number[][] = [];
     const digits = [...this.series].map((n) => Number(n));
@@ -20,5 +15,14 @@ export class Series {
       digits.shift();
     }
     return allSeries;
+  }
+
+  private validateSliceLength(sliceLength: number): void {
+    if (sliceLength > this.series.length)
+      throw new Error('slice length cannot be greater than series length');
+
+    if (sliceLength === 0) throw new Error('slice length cannot be zero');
+
+    if (sliceLength < 0) throw new Error('slice length cannot be negative');
   }
 }
