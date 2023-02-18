22cedd3d08265de0a16a86b29080d17846e00311
GoFightNguyen <jsn.dev@outlook.com>
2023-02-18 10:49:12 -0700

    exercise(roman-numerals): extract Map

--- exercises/typescript/roman-numerals/roman-numerals.ts
+++ exercises/typescript/roman-numerals/roman-numerals.ts
@@ -1,63 +1,27 @@
+const NORMAL_TO_ROMAN_MAPPING = new Map<number, string>([
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I'],
+]);
+
 export const toRoman = (normal: number): string => {
   let roman = '';
 
-  while (Math.floor(normal / 1000) > 0) {
-    roman += 'M';
-    normal -= 1000;
-  }
-
-  while (Math.floor(normal / 900) > 0) {
-    roman += 'CM';
-    normal -= 900;
-  }
-
-  while (Math.floor(normal / 500) > 0) {
-    roman += 'D';
-    normal -= 500;
-  }
-
-  while (Math.floor(normal / 400) > 0) {
-    roman += 'CD';
-    normal -= 400;
-  }
-  while (Math.floor(normal / 100) > 0) {
-    roman += 'C';
-    normal -= 100;
-  }
-  while (Math.floor(normal / 90) > 0) {
-    roman += 'XC';
-    normal -= 90;
-  }
-
-  while (Math.floor(normal / 50) > 0) {
-    roman += 'L';
-    normal -= 50;
-  }
-
-  if (Math.floor(normal / 40) > 0) {
-    roman += 'XL';
-    normal -= 40;
-  }
-  while (Math.floor(normal / 10) > 0) {
-    roman += 'X';
-    normal -= 10;
-  }
-  if (normal === 9) {
-    roman += 'IX';
-    normal -= 9;
-  }
-
-  if (normal >= 5) {
-    roman += 'V';
-    normal -= 5;
-  }
-  if (normal === 4) {
-    roman += 'IV';
-    normal -= 4;
-  }
-  while (normal > 0) {
-    roman += 'I';
-    normal--;
+  for (const [x, y] of NORMAL_TO_ROMAN_MAPPING.entries()) {
+    while (Math.floor(normal / x) > 0) {
+      roman += y;
+      normal -= x;
+    }
   }
 
   return roman;
