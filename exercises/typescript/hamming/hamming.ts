c4d13b01a71bc41240c3057b37bad561e4109320
GoFightNguyen <jsn.dev@outlook.com>
2023-02-19 13:01:52 -0700

    exercise(hammming): refator to reduce()

--- exercises/typescript/hamming/hamming.ts
+++ exercises/typescript/hamming/hamming.ts
@@ -2,9 +2,9 @@ export function compute(left: string, right: string): number {
   if (left.length !== right.length)
     throw new Error('DNA strands must be of equal length.');
 
-  let hammingDistance = 0;
-  [...left].forEach((l, index) => {
-    if (l !== right[index]) hammingDistance++;
-  });
-  return hammingDistance;
+  return [...left].reduce(
+    (hammingDistance, letter, index) =>
+      (hammingDistance += letter === right[index] ? 0 : 1),
+    0
+  );
 }
