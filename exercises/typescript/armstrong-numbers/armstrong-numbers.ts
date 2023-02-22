81cfdb851882172c142d2696888c2d15719f8744
GoFightNguyen <jsn.dev@outlook.com>
2023-02-21 18:59:38 -0700

    exercise(armstrong-numbers): extract getDigits

--- exercises/typescript/armstrong-numbers/armstrong-numbers.ts
+++ exercises/typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,8 +1,12 @@
+function getDigits(value: number): number[] {
+  return [...String(value)].map((n) => Number(n));
+}
+
 export function isArmstrongNumber(value: number): boolean {
-  const stringified = String(value);
-  const numberOfDigits = stringified.length;
-  const x = [...stringified]
-    .map((n) => Number(n) ** numberOfDigits)
+  const digits = getDigits(value);
+  const numberOfDigits = digits.length;
+  const x = digits
+    .map((n) => n ** numberOfDigits)
     .reduce((agg, curr) => (agg += curr), 0);
   return x === value;
 }
